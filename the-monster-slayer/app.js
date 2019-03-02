f0e9b8806cff44315434710e8296d4ea2703b1cc
David <dodwei.dev@gmail.com>
2019-03-02 10:44:50 +0800

    Refactor code

--- the-monster-slayer/app.js
+++ the-monster-slayer/app.js
@@ -11,30 +11,53 @@ new Vue({
             this.playerHealth = 100;
             this.monsterHealth = 100;
         },
+
         attack: function() {
             var max = 10;
             var min = 3;
-            var damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            var damage = this.calculateDamage(max, min);
             this.monsterHealth -= damage;
 
-            if (this.monsterHealth <= 0) {
-                alert("You won!");
-                this.gameIsRunning = false;
+            if (this.checkWin()) {
                 return;
             }
 
             max = 12;
             min = 5;
-            damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            damage = this.calculateDamage(max, min);
             this.playerHealth -= damage;
 
-            if (this.playerHealth <= 0) {
-                alert("Monster won!");
-                this.gameIsRunning = false;
-            }
+            this.checkWin();
         },
+
         specialAttack: function() {},
+
         heal: function() {},
-        giveUp: function() {}
+
+        giveUp: function() {},
+
+        calculateDamage: function(max, min) {
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+
+        checkWin: function() {
+            if (this.monsterHealth <= 0) {
+                if (confirm("You won! New Game?")) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm("You lost!")) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+
+            return false;
+        }
     }
 });
