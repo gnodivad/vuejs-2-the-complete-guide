a2430776b6b223da57599b80efde31f6e1c96754
David <dodwei.dev@gmail.com>
2019-03-05 14:52:28 +0800

    The root component in App.vue file

--- vue-cli/src/App.vue
+++ vue-cli/src/App.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>Hello World!</h1>
+  <p>Server status: {{ status }}</p>
 </template>
 
 <script>
-export default {};
+export default {
+  data: function() {
+    return {
+      status: "Critical"
+    };
+  }
+};
 </script>
 
 <style>
