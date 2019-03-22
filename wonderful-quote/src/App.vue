7c85c501c9291098b51e0c7513114e0e66818ead
David <dodwei.dev@gmail.com>
2019-03-22 08:58:06 +0800

    Passing fata with props and slots

--- wonderful-quote/src/App.vue
+++ wonderful-quote/src/App.vue
@@ -1,14 +1,21 @@
 <template>
-    <div class="container"></div>
+    <div class="container">
+        <app-quote-grid :quotes="quotes"></app-quote-grid>
+    </div>
 </template>
 
 <script>
+import QuoteGrid from "./components/QuoteGrid.vue";
+
 export default {
     data: function() {
         return {
             quotes: ["Just a Quote to see something"],
             maxQuotes: 10,
         };
+    },
+    components: {
+        appQuoteGrid: QuoteGrid,
     },
 };
 </script>
