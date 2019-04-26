24fcf8db1fd29e45e2d7c4741c3a842d12980f28
David <dodwei.dev@gmail.com>
2019-04-26 10:21:54 +0800

    Using radio buttons

--- forms/src/App.vue
+++ forms/src/App.vue
@@ -82,10 +82,11 @@
           class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 form-group"
         >
           <label for="male">
-            <input type="radio" id="male" value="Male" /> Male
+            <input type="radio" id="male" value="Male" v-model="gender" /> Male
           </label>
           <label for="female">
-            <input type="radio" id="female" value="Female" /> Female
+            <input type="radio" id="female" value="Female" v-model="gender" />
+            Female
           </label>
         </div>
       </div>
@@ -124,7 +125,7 @@
             <ul>
               <li :key="index" v-for="(item, index) in sendMail">{{ item }}</li>
             </ul>
-            <p>Gender:</p>
+            <p>Gender: {{ gender }}</p>
             <p>Priority:</p>
             <p>Switched:</p>
           </div>
@@ -144,7 +145,8 @@ export default {
         age: 27
       },
       message: "A new text",
-      sendMail: []
+      sendMail: [],
+      gender: "Male"
     };
   }
 };
