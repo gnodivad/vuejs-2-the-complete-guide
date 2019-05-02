2b46eeb50154340624c9c2f727f70fb9e8a18253
David <dodwei.dev@gmail.com>
2019-05-02 10:29:41 +0800

    Understanding mixins

--- filter-and-mixin/src/App.vue
+++ filter-and-mixin/src/App.vue
@@ -11,12 +11,15 @@
             {{ fruit }}
           </li>
         </ul>
+        <hr />
+        <app-list></app-list>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import List from "./List.vue";
 export default {
   data() {
     return {
@@ -36,6 +39,9 @@ export default {
         return fruit.match(this.filterText);
       });
     }
+  },
+  components: {
+    appList: List
   }
 };
 </script>
---
+++ filter-and-mixin/src/List.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h1>Filters & Mixins</h1>
+    <input v-model="filterText" />
+    <ul>
+      <li :key="index" v-for="(fruit, index) in filteredFruits">
+        {{ fruit }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      fruits: ["Apple", "Banana", "Mango", "Melon"],
+      filterText: ""
+    };
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter(fruit => {
+        return fruit.match(this.filterText);
+      });
+    }
+  }
+};
+</script>
+
+<style>
+</style>
