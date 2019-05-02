c03e2512a49f797461cf4516861940834196e759
David <dodwei.dev@gmail.com>
2019-05-02 10:22:14 +0800

    Alternative to filters

--- filter-and-mixin/src/App.vue
+++ filter-and-mixin/src/App.vue
@@ -4,6 +4,13 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
         <p>{{ text | toUppercase | toLowercase }}</p>
+        <hr />
+        <input v-model="filterText" />
+        <ul>
+          <li :key="index" v-for="(fruit, index) in filteredFruits">
+            {{ fruit }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -13,12 +20,21 @@
 export default {
   data() {
     return {
-      text: "Hello there!"
+      text: "Hello there!",
+      fruits: ["Apple", "Banana", "Mango", "Melon"],
+      filterText: ""
     };
   },
   filters: {
     toUppercase(value) {
       return value.toUpperCase();
+    }
+  },
+  computed: {
+    filteredFruits() {
+      return this.fruits.filter(fruit => {
+        return fruit.match(this.filterText);
+      });
     }
   }
 };
