ffc040529f697ef9f342d16b862c39231ed5f620
David <dodwei.dev@gmail.com>
2019-05-23 15:28:27 +0800

    Setting up an initial animation

--- animations-and-transitions/src/App.vue
+++ animations-and-transitions/src/App.vue
@@ -15,6 +15,9 @@
         <transition name="slide" type="animation">
           <div class="alert alert-info" v-if="show">This is some info</div>
         </transition>
+        <transition name="fade" appear>
+          <div class="alert alert-info" v-if="show">This is some info</div>
+        </transition>
       </div>
     </div>
   </div>
@@ -24,7 +27,7 @@
 export default {
   data() {
     return {
-      show: false
+      show: true
     };
   }
 };
