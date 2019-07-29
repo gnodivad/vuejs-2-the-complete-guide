4914f15544beda9d81e17d4d477708772f35662b
David <dodwei.dev@gmail.com>
2019-07-29 09:28:44 +0800

    [Vue resource]GETting and Transforming Data (Sending a GET Request)

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -12,6 +12,14 @@
           <input class="form-control" type="text" v-model="user.email" />
         </div>
         <button class="btn btn-primary" @click="submit">Submit</button>
+        <hr />
+        <button class="btn btn-primary" @click="fetchData">Get Data</button>
+        <br /><br />
+        <ul class="list-group">
+          <li class="list-group-item" v-for="(u, index) in users" :key="index">
+            {{ u.username }} - {{ u.email }}
+          </li>
+        </ul>
       </div>
     </div>
   </div>
@@ -24,7 +32,8 @@ export default {
       user: {
         username: "",
         email: ""
-      }
+      },
+      users: []
     };
   },
   methods: {
@@ -39,6 +48,22 @@ export default {
             console.log(error);
           }
         );
+    },
+    fetchData() {
+      this.$http
+        .get("https://vuejs-http-cd11b.firebaseio.com/data.json")
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          const resultArray = [];
+
+          for (let key in data) {
+            resultArray.push(data[key]);
+          }
+
+          this.users = resultArray;
+        });
     }
   }
 };
