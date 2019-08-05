cbdab27b218579ce4c1fd0edee04a4d402cf9919
David <dodwei.dev@gmail.com>
2019-08-05 09:15:22 +0800

    [Vue resource]Creating Custom Resources

--- vue-resource/src/App.vue
+++ vue-resource/src/App.vue
@@ -47,7 +47,8 @@ export default {
       //     console.log(error);
       //   }
       // );
-      this.resource.save({}, this.user);
+      // this.resource.save({}, this.user);
+      this.resource.saveAlt(this.user);
     },
     fetchData() {
       this.$http
@@ -67,7 +68,10 @@ export default {
     }
   },
   created() {
-    this.resource = this.$resource("data.json");
+    const customActions = {
+      saveAlt: { method: "POST", url: "alternative.json" }
+    };
+    this.resource = this.$resource("data.json", {}, customActions);
   }
 };
 </script>
