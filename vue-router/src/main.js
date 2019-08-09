858134acf44a332d67230910a7e55dbfe719585d
David <dodwei.dev@gmail.com>
2019-08-09 08:34:59 +0800

    [Vue-router]Fix incorrect route file

--- vue-router/src/main.js
+++ vue-router/src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
 import App from './App.vue'
-import { routes } from './routes';
+import { routes } from './routes.js';
 
 Vue.use(VueRouter);
 
